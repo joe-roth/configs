9d5dbd361e5b1f183b9e80aa3273b229845c39a4
Joe Roth <jroth@gmail.com>
2018-03-25 21:09:56 -0400

    Add neovim config

--- xmonad/xfce/xmonad.hs
+++ xmonad/xfce/xmonad.hs
@@ -38,8 +38,7 @@ main = xmonad $ xfceConfig {
 }
                 `additionalKeys`
                  [ ((myModMask, xK_f        ), spawn "firefox")
-                 , ((myModMask, xK_c        ), spawn "google-chrome --touch-devices=123")
-                 , ((myModMask, xK_c        ), spawn "chromium-browser")
+                 , ((myModMask, xK_c        ), spawn "google-chrome")
                  , ((myModMask, xK_o        ), spawn "xfce4-appfinder")
                  , ((myModMask, xK_Return        ), spawn "exo-open --launch TerminalEmulator")
                  , ((myModMask, xK_a        ), sendMessage MirrorShrink )
